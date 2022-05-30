b3973d5a631cbeb00fb972927d46960f9ca8d27f
Nathally Nunes <nathally.nunes@gmail.com>
2022-05-30 12:03:52 -0300

    Exercicios Aleatorios - Java

---
+++ src/parte2/exercicios/poo/Ex3_ExbindoNumerosImpares.java
@@ -0,0 +1,24 @@
+package parte2.exercicios.poo;
+/*
+Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até
+X, um valor por linha, inclusive o X, se for o caso.
+- Entrada
+O arquivo de entrada contém 1 valor inteiro qualquer.
+- Saída
+Imprima todos os valores ímpares de 1 até X, inclusive X, se for o caso.
+*/
+
+import java.util.Scanner;
+
+public class Ex3_ExbindoNumerosImpares {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int num = entrada.nextInt();
+
+        for (int i = 0; i <= num; i++) {
+            if (i % 2 == 1) {
+                System.out.println(i);
+            }
+        }
+    }
+}
---
+++ src/parte2/exercicios/poo/Main.java
@@ -0,0 +1,8 @@
+package parte2.exercicios.poo;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
